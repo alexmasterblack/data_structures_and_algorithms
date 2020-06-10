d1584a5522a29c952e24190b038238460e27c151
master <58977579+alexmasterblack@users.noreply.github.com>
2020-06-10 20:52:27 +1000

    Create avl_tree.cpp

---
+++ structures/avl_tree.cpp
@@ -0,0 +1,170 @@
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class AvlTree {
+    struct node {
+        int value;
+        node *left;
+        node *right;
+        int height;
+    };
+
+    node *root;
+
+    int height(node *top) {
+        if (top == NULL)
+            return 0;
+        else
+            return top->height;
+    }
+
+    int balance(node *top) {
+        if (top == NULL)
+            return 0;
+        else
+            return height(top->right) - height(top->left);
+    }
+
+    node *right_rotation(node *top) {
+        node *new_top = top->left; //новым узлом становится левый потомок
+        top->left = new_top->right;
+        new_top->right = top;
+        top->height = max(height(top->left), height(top->right)) + 1;
+        new_top->height = max(height(new_top->left), height(new_top->right)) + 1;
+        return new_top;
+    }
+
+    node *left_rotation(node *top) {
+        node *new_top = top->right; //новым узлом становится правый потомок
+        top->right = new_top->left; //левые потомки  нового узла становятся правыми старого
+        new_top->left = top; //старая вершина теперь левый потомок новой
+        top->height = max(height(top->left), height(top->right)) + 1;
+        new_top->height = max(height(new_top->left), height(new_top->right)) + 1;
+        return new_top;
+    }
+
+    node *balance_check(node *top) {
+        //высота вершины
+        top->height = max(height(top->left), height(top->right)) + 1;
+        if (balance(top) == -2) {
+            if (balance(top->left) == -1)
+                return right_rotation(top);
+            else if (balance(top->left) == 1) {
+                top->left = left_rotation(top->left);
+                return right_rotation(top);
+            }
+        }
+        if (balance(top) == 2) {
+            if (balance(top->right) == 1)
+                return left_rotation(top);
+            else if (balance(top->right) == -1) {
+                top->right = right_rotation(top->right);
+                return left_rotation(top);
+            }
+        }
+        return top;
+    }
+
+    node *minumum(node *top) {
+        if (top == NULL)
+            return 0;
+        if (top->left != NULL)
+            return minumum(top->left);
+        else return top;
+    }
+
+    node *push(int key, node *top) {
+        if (top == NULL) {
+            top = new node;
+            top->value = key;
+            top->left = NULL;
+            top->right = NULL;
+        } else if (key < top->value) {
+            top->left = push(key, top->left);
+        } else if (key > top->value) {
+            top->right = push(key, top->right);
+        }
+        return balance_check(top);
+    }
+
+    node *pop(int key, node *top) {
+        if (top == NULL) {
+            return top;
+        }
+        if (key < top->value) {
+            top->left = pop(key, top->left);
+        } else if (key > top->value) {
+            top->right = pop(key, top->right);
+        } else if (top->left != NULL && top->right != NULL) {
+            top->value = minumum(top->right)->value;
+            top->right = pop(minumum(top->right)->value, top->right);
+        } else {
+            if (top->left != NULL)
+                return balance_check(top->left);
+            else if (top->right != NULL)
+                return balance_check(top->right);
+            else return NULL;
+        }
+        return balance_check(top);
+    }
+
+    int lenght(node *top, int left_size = 0, int right_size = 0) {
+        if (top == NULL) {
+            return 0;
+        } else {
+            if (top->left != NULL)
+                left_size = lenght(top->left);
+            if (top->right != NULL)
+                right_size = lenght(top->right);
+            return left_size + right_size + 1;
+        }
+    }
+
+public:
+    AvlTree() {
+        root = NULL;
+    }
+
+    void push(int key) {
+        root = push(key, root);
+    }
+
+    void pop(int key) {
+        root = pop(key, root);
+    }
+
+    int lenght() {
+        return lenght(root);
+    }
+
+    int min() {
+        return minumum(root)->value;
+    }
+};
+
+int main() {
+    ifstream fin("input.txt");
+    ofstream fout("output.txt");
+
+    AvlTree tree;
+
+    int sup;
+    while (fin >> sup) {
+        if (sup > 0)
+            tree.push(sup);
+        if (sup < 0)
+            tree.pop(-sup);
+        if (sup == 0)
+            break;
+    }
+
+    int size = tree.lenght();
+    for (int i = 0; i < size; i++) {
+        sup = tree.min();
+        fout << sup << ' ';
+        tree.pop(sup);
+    }
+}
